ee2476962581bd9cc978bdb377ada6c9a0046eb5
dtmey125 <144493295+dtmey125@users.noreply.github.com>
2024-06-28 15:04:55 -0700

    Implement basic order management system

--- src/interface.py
+++ src/interface.py
@@ -1,19 +1,26 @@
 
 from flask import Flask, render_template, url_for, request, Blueprint
 from flask_login import login_user, login_required, logout_user, current_user
-from models import db, User, Item
-
+from models import db, User, Item, Order
+import datetime
 interface_bp = Blueprint("interface_bp", __name__)
 
 @interface_bp.route('/')
 @interface_bp.route('/home')
 def home_page():
+    #Order.addOrder(datetime.datetime.now(), datetime.datetime.now(), "Shipped", "1x3,4x6,2x82,3x4", "Test recipient", "1234 Street Rd.")
+
     return render_template("homepage.html", title="Home")
 
 @interface_bp.route('/add_donation')
 @login_required
 def donation_pg():
     return render_template("donation.html", title="Add Donation")
+
+@interface_bp.route('/add_order')
+@login_required
+def add_order_pg():
+    return render_template("add_order.html", title="Add Order")
 
 @interface_bp.route('/inventory')
 @login_required
@@ -28,10 +35,15 @@ def inventory_pg():
 def report_pg():
     return render_template("report.html", title="Report")
 
-@interface_bp.route('/transaction')
+@interface_bp.route('/orders')
 @login_required
-def transaction_pg():
-    return render_template("transaction.html", title="Order Now")
+def orders_pg():
+    orders_list = Order.getAllOrders()
+    itemlists = {}
+    for order in orders_list: # populate dictionary
+        itemlists[order.id] = order.getOrderItems(order.id)
+    
+    return render_template("orders.html", title="Orders", orders = orders_list, itemlists = itemlists)
 
 @interface_bp.route('/account')
 @login_required
